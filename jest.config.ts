1ebb50fcadf6ad9864fde2dfab264241434c8e39
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2025-03-31 21:30:54 +0300

    fix(jest): fix ignored path pattern for e2e tests

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: { "^@/(.*)$": "<rootDir>/src/$1" },
-  modulePathIgnorePatterns: ["<rootDir>/tests/"],
+  modulePathIgnorePatterns: ["<rootDir>/e2e/"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
